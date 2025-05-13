3d2217ba2c984e311e999d046a721bd7af2cbdaf
Torito77 <jorge.toro.mdz@gmail.com>
2025-05-12 22:51:54 -0600

    Fix arduino autorecconect

--- arduino_manager.py
+++ arduino_manager.py
@@ -5,7 +5,7 @@ from database import db, register_readings
 
 SERIAL_PORT = 'COM3'  
 BAUD_RATE = 9600
-
+RECONNECT_INTERVAL = 5
 class Duino_Manager:
     
     def get_datos(self):
@@ -15,32 +15,34 @@ class Duino_Manager:
         self.arduino = None
         self.datos: dict 
         
-        def connect_to_arduino():
+        def connect_and_read():
+            
             while True:
-                try:
-                    self.arduino = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
-                except serial.SerialException as e:
-                    print(f"Error al abrir el puerto COM3: {e}")
-                    self.arduino = None
-                    time.sleep(1)
-                else:
-                    print("Connected succesfully")
-                    break
-        
-        
-        def read_arduino():
-            while True:
-                if self.arduino and self.arduino.in_waiting:
-                    linea = self.arduino.readline().decode('utf-8').strip()
+                if self.arduino is None or not self.arduino.is_open:
                     try:
-                        self.datos = dict( item.split(":") for item in linea.split(",") )
-                    except Exception as e:
-                        print(f"Error procesando datos: {e}")
-                    else:
-                        register_readings(self.datos)
-                
-                # FIXME: This value may vary
+                        self.arduino = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1)
+                        print(f"Conectado exitosamente al puerto {SERIAL_PORT}")
+                        
+                    except serial.SerialException as e:
+                        print(f"No se pudo conectar al puerto {SERIAL_PORT}: {e}")
+                        self.arduino = None
+                        time.sleep(RECONNECT_INTERVAL)
+                        continue 
+
+                try:
+                    if self.arduino.in_waiting:
+                        linea = self.arduino.readline().decode('utf-8').strip()
+                        try:
+                            self.datos = dict(item.split(":") for item in linea.split(","))
+                            register_readings(self.datos)
+                        except Exception as e:
+                            print(f"Error procesando datos: {e}")
+                            
+                except (serial.SerialException, OSError) as e:
+                    print(f"Conexión perdida: {e}")
+                    self.arduino = None
+                    time.sleep(RECONNECT_INTERVAL)
+
                 time.sleep(1)
-    
-        Thread(target=connect_to_arduino, daemon=True).start()
-        Thread(target=read_arduino, daemon=True).start()
+        
+                Thread(target=connect_and_read, daemon=True).start()
